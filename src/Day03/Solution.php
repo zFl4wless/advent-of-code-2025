8be1b95e5ddd04a33fdb672f717904e66e225b91
fl4wless <noah.bauditz@live.de>
2025-12-03 08:10:03 +0100

    feature: solves day 3 (part 1)

--- src/Day03/Solution.php
+++ src/Day03/Solution.php
@@ -1,23 +1,47 @@
 <?php
+
 namespace AOC2025\Day03;
+
 require_once __DIR__ . '/../../vendor/autoload.php';
+
 use AOC2025\AbstractSolution;
+
 class Solution extends AbstractSolution
 {
     public function solvePart1(string $input): mixed
     {
-        // TODO: Implement part 1 solution
         $lines = explode("\n", $input);
-        return 0;
+
+        $jolts = [];
+
+        foreach ($lines as $line) {
+            if (empty($line)) {
+                continue;
+            }
+
+            $batteries = str_split($line);
+            $maxJoltage = 0;
+
+            for ($i = 0; $i < count($batteries) - 1; $i++) {
+                for ($j = $i + 1; $j < count($batteries); $j++) {
+                    $joltage = (int)($batteries[$i] . $batteries[$j]);
+                    $maxJoltage = max($maxJoltage, $joltage);
+                }
+            }
+
+            $jolts[] = $maxJoltage;
+        }
+
+        return array_sum($jolts);
     }
-    public function solvePart2(string $input): mixed
+
+    public function solvePart2(string $input): int
     {
-        // TODO: Implement part 2 solution
         $lines = explode("\n", $input);
         return 0;
     }
 }
-// Run solution if this file is executed directly
+
 if (basename(__FILE__) === basename($_SERVER['PHP_SELF'])) {
     $solution = new Solution();
     echo "Day 03:\n";
