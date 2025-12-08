546b09290ed9bab53c5829950d0c7c732861e56d
fl4wless <noah.bauditz@live.de>
2025-12-08 08:11:53 +0100

    feature: solves day 04

--- src/Day04/Solution.php
+++ src/Day04/Solution.php
@@ -1,23 +1,108 @@
 <?php
+
 namespace AOC2025\Day04;
 require_once __DIR__ . '/../../vendor/autoload.php';
+
 use AOC2025\AbstractSolution;
+use AOC2025\Utils;
+
 class Solution extends AbstractSolution
 {
     public function solvePart1(string $input): mixed
     {
-        // TODO: Implement part 1 solution
-        $lines = explode("\n", $input);
-        return 0;
+        $result = 0;
+
+        $grid = Utils::parseGrid($input);
+        $maxCol = count($grid);
+        foreach ($grid as $y => $row) {
+            $maxRow = count($row);
+
+            foreach ($row as $x => $value) {
+                if ($value !== '@') {
+                    continue;
+                }
+
+                $neighbours = 0;
+                for ($dy = -1; $dy <= 1; $dy++) {
+                    for ($dx = -1; $dx <= 1; $dx++) {
+                        if ($dy === 0 && $dx === 0) {
+                            continue;
+                        }
+
+                        $nx = $x + $dx;
+                        $ny = $y + $dy;
+                        if ($nx >= 0 && $nx < $maxRow && $ny >= 0 && $ny < $maxCol) {
+                            if ($grid[$ny][$nx] === '@') {
+                                $neighbours++;
+                            }
+                        }
+                    }
+                }
+
+                if ($neighbours < 4) {
+                    $result++;
+                }
+            }
+        }
+
+        return $result;
     }
+
     public function solvePart2(string $input): mixed
     {
-        // TODO: Implement part 2 solution
-        $lines = explode("\n", $input);
-        return 0;
+        $grid = Utils::parseGrid($input);
+        $totalRemoved = 0;
+
+        while (true) {
+            $toRemove = [];
+            $maxCol = count($grid);
+
+            foreach ($grid as $y => $row) {
+                $maxRow = count($row);
+
+                foreach ($row as $x => $value) {
+                    if ($value !== '@') {
+                        continue;
+                    }
+
+                    $neighbours = 0;
+                    for ($dy = -1; $dy <= 1; $dy++) {
+                        for ($dx = -1; $dx <= 1; $dx++) {
+                            if ($dy === 0 && $dx === 0) {
+                                continue;
+                            }
+
+                            $nx = $x + $dx;
+                            $ny = $y + $dy;
+                            if ($nx >= 0 && $nx < $maxRow && $ny >= 0 && $ny < $maxCol) {
+                                if ($grid[$ny][$nx] === '@') {
+                                    $neighbours++;
+                                }
+                            }
+                        }
+                    }
+
+                    if ($neighbours < 4) {
+                        $toRemove[] = [$y, $x];
+                    }
+                }
+            }
+
+            if (empty($toRemove)) {
+                break;
+            }
+
+            foreach ($toRemove as [$y, $x]) {
+                $grid[$y][$x] = '.';
+            }
+
+            $totalRemoved += count($toRemove);
+        }
+
+        return $totalRemoved;
     }
 }
-// Run solution if this file is executed directly
+
 if (basename(__FILE__) === basename($_SERVER['PHP_SELF'])) {
     $solution = new Solution();
     echo "Day 04:\n";
